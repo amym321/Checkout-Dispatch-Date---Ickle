e762dea08dff9db37af7f766b9b85e0dfc8ab65e
Amy Mick <amymick847@gmail.com>
2023-07-23 17:59:02 -0500

    color change

--- extensions/checkout-ui-ext/src/index.tsx
+++ extensions/checkout-ui-ext/src/index.tsx
@@ -59,7 +59,7 @@ function App() {
   const dateDispatchedText = date ? `Order to be dispatched by ${date}.` : "";
   return (
     <BlockStack inlineAlignment="start">
-      <Text size="base" emphasis="bold">
+      <Text size="base" emphasis="bold" style={{color: "#4047B"}}>
         {dateDispatchedText}
       </Text>
     </BlockStack>
