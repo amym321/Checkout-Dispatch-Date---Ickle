cc767e2fa0f9b9de27aa2d2ab780b8650a11e773
Amy Mick <amymick847@gmail.com>
2023-07-23 19:52:27 -0500

    remove .

--- extensions/checkout-ui-ext/src/index.tsx
+++ extensions/checkout-ui-ext/src/index.tsx
@@ -56,7 +56,7 @@ function App() {
     date = `${dd}/${mm}`;
   }
   console.log("Attribute 1:", attributes);
-  const dateDispatchedText = date ? `Order to be dispatched by ${date}.` : "";
+  const dateDispatchedText = date ? `Order to be dispatched by ${date}` : "";
   return (
     <BlockStack inlineAlignment="start">
       <Text size="base" emphasis="bold" style={{color: "#4047B"}}>
